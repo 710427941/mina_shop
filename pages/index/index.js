6a50b4c38680c65d2344ba8b3eaabc78fe73c4cb
DESKTOP-LJ9E7IS\strong <710427941@qq.com>
2018-07-31 17:08:05 +0800

    [增加]首页广告

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,16 +10,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    banner:[],
+    banner: [],
     domian: app.globalData.domian + 'uploads/',
-    randmos:'',
-    ad:[]
+    randmos: '',
+    ad: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.loadIndex()
     this.createRandom()
     this.loadAd()
@@ -28,36 +28,35 @@ Page({
     var url = 'Index/index'
     var params = {}
     var method = 'get'
-    service.service(url,params,method,data=>{
-      if(data.code == 200 && data.result){
+    service.service(url, params, method, data => {
+      if (data.code == 200 && data.result) {
         console.log(data.result)
         this.setData({
-          banner:data.result
+          banner: data.result
         })
       }
-    }, data => { }, data => { })
+    }, data => {}, data => {})
   },
   loadAd() {
-    var url = 'Index/indexAd' 
+    var url = 'Index/indexAd'
     var params = {}
     var method = 'get'
-    service.service(url, params, method,data=>{
+    service.service(url, params, method, data => {
       console.log(data.result)
-      if(data.code == 200 && data.result){
+      if (data.code == 200 && data.result) {
         this.setData({
-          ad:data.result
+          ad: data.result
         })
       }
-    }, data => { }, data => { })
+    }, data => {}, data => {})
   },
   createRandom() {
     this.setData({
       randmos: util.randmo()
     })
-  }, 
+  },
   search(e) {
     console.log(e)
   },
-  searchGoods() {
-  }
+  searchGoods() {}
 })
